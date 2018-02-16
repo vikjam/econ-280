4e413bfffdf616944f92e27938fa5531bbeb0b2b
Shunsuke-Hori <hori0518@gmail.com>
2018-02-16 10:44:01 -0800

    bunch of update for exercise 2 (#2)
    * some updates
    
    * I like function
    
    * I like four spaces for indent
    
    * I use space instead of tab

--- exercise02.jl
+++ exercise02.jl
@@ -18,24 +18,22 @@ const μ_y = c / (1 - ρ);
 
 # Part (b)
 function rand_ar(T)
-	ϵ = rand(Normal(μ, σ²), T);
-	y = fill(NaN, T);
-
-	y[1] = μ_y + rand(Normal(μ, σ²));
-	for t = 2:T
-		y[t] = c + ρ * y[t - 1] + ϵ[t];
-	end
-
-	return(y)
+    ϵ = rand(Normal(μ, σ²), T);
+    y = fill(NaN, T);
+    y[1] = μ_y + rand(Normal(μ, σ²));
+    [y[t] = c + ρ * y[t - 1] + ϵ[t] for t in 1:T]
+    return(y)
 end
 
-y_100    = rand_ar(100);
-μ_100    = mean(y_100);
-γ_100    = autocov(y_100, [1]);
-γ_100alt = (1 / (100 - 1)) * (y_100[2:100] - μ_100)' * (y_100[1:99] - μ_100);
+function compute_autocov(T)
+    y    = rand_ar(T)
+    μ    = mean(y)
+    γ    = autocov(y, [1])
+    γalt = (1 / (T - 1)) * dot(y[2:T] - μ, y[1:T-1] - μ)
+    return (γ, γalt)
+end
+
+compute_autocov(100)
 
 # Part (c)
-y_10K    = rand_ar(10_000);
-μ_10K    = mean(y_10K);
-γ_10K    = autocov(y_10K, [1]);
-γ_10Kalt = (1 / (10_000 - 1)) * (y_10K[2:10_000] - μ_10K)' * (y_10K[1:(10_000 - 1)] - μ_10K);
+compute_autocov(10_000)
